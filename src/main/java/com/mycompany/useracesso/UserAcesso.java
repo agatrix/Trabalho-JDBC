17291f15e3befcd7623881e3ba8e41510596eabc
agatrix <gustavo.l.mav@gmail.com>
2024-09-07 14:15:14 -0300

    inicializar projeto

---
+++ src/main/java/com/mycompany/useracesso/UserAcesso.java
@@ -0,0 +1,12 @@
+package com.mycompany.useracesso;
+
+/**
+ *
+ * @author gusta
+ */
+public class UserAcesso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
